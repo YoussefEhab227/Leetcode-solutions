1595a8a725cf3df163ae608151f4bfdd8c968365
YoussefEhab227 <43238167+YoussefEhab227@users.noreply.github.com>
2022-06-25 22:49:17 +0200

    Time: 94 ms (83.45%), Space: 24 MB (21.62%) - LeetHub

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+       vector<vector<int>> tS;
+        vector<int> f;
+        int start,curr_sum,target,end;
+        int n = nums.size();
+        
+        if(n<3) return tS;
+       
+        sort(nums.begin(),nums.end());
+        
+        if((nums[0]==0)&&(nums[n-1]==0))
+        {
+            f.push_back(nums[0]);
+            f.push_back(nums[0]);
+            f.push_back(nums[0]);
+            tS.push_back(f);  
+            f.clear();
+            return tS;
+        }
+        
+        int i;
+        for( i=0;i<nums.size()&&nums[i]<=0;i++)
+        {
+            if(i+1==nums.size()){break;}
+            start=i+1;
+            end=nums.size()-1;
+            target=-nums[i];
+            while(start!=end)
+            {
+                curr_sum=nums[start]+nums[end];
+                if(curr_sum<target)
+                {
+                    start++;
+                }
+                 else if(curr_sum==target)
+                {
+                    f.push_back(nums[i]);
+                    f.push_back(nums[start]);
+                    f.push_back(nums[end]);
+                    tS.push_back(f);
+                    start++;
+                    f.clear();
+                      }
+                else
+                {
+                    end--;
+                }
+                
+            }
+            while(i<nums.size()&&nums[i+1]==nums[i])
+            {
+                i++;
+            }
+        }
+        tS.erase(unique(tS.begin(), tS.end()), tS.end());    
+        return tS;
+    }
+};
