005e90ecc53c45e9fa0ad9f128f941d4c73e4fb4
YoussefEhab227 <43238167+YoussefEhab227@users.noreply.github.com>
2022-07-11 22:21:38 +0200

    Time: 65 ms (9.09%), Space: 31.1 MB (87.50%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,15 +1,24 @@
-class Solution(object):
-    def pivotIndex(self, nums):
-        left_sum, right_sum, total_sum = 0,0,0
-        for window_end in range(len(nums)):
-            total_sum += nums[window_end]
-        right_sum = total_sum - nums[0]
-        if right_sum == 0:
-            return 0
-        for idx in range(1, len(nums)):
-            left_sum += nums[idx - 1]
-            right_sum -= nums[idx]
-            if left_sum == right_sum:
-                return idx
-        return -1
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+    
+    int left_sum= 0, right_sum=0;
+    
+    int total=accumulate(nums.begin(),nums.end(),0); 
+     
+    right_sum = total - nums[0];
+    
+    if(right_sum == 0) return 0;
+        
+    for(int i = 1; i<nums.size(); i++)
+        {
+            right_sum-= nums[i];
+            left_sum += nums[i-1];
+            if(right_sum==left_sum) return i;
+        }
+        return -1;
+    }
+};
+
+
         
