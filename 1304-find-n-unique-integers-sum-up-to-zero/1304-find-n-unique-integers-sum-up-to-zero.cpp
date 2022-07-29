5291685440ee0b81d3b6ebdcf3a80b9944f6d472
YoussefEhab227 <43238167+YoussefEhab227@users.noreply.github.com>
2022-07-29 02:06:56 +0200

    Time: 0 ms (100.00%), Space: 6.7 MB (80.95%) - LeetHub

---
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> sumZero(int n){
+        int s=n/2;
+        vector<int> ans;
+        if(n%2==1)
+        {
+            
+            for(int i=0;i<n;i++)
+            {
+                ans.push_back(i-s);
+            }
+        }
+        else 
+        {
+            for(int j=0;j<=n;j++)
+            {   if((j-s)!=0)
+                ans.push_back((j-s));
+            }
+        }
+     return ans;
+    
+    }
+};
