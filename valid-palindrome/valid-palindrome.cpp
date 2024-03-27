978db8fbeb2ec98ddfa14815f15b42faa627966e
YoussefEhab227 <43238167+YoussefEhab227@users.noreply.github.com>
2024-03-28 01:10:48 +0200

    Time: 3 ms, Memory: 8.8 MB - LeetHub

--- valid-palindrome/valid-palindrome.cpp
+++ valid-palindrome/valid-palindrome.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-       int n=s.length();
+        
+        int n=s.length();
         vector<char> str(n);
         int j=0;
-
         for(int i=0;i<n;i++)
         {
             if(isalnum(s[i]))
@@ -12,16 +12,11 @@ public:
                 str[j++]=tolower(s[i]);
             }
         }
-         int l=str.size();
-
         for(int i=0;i<j/2;i++)
         {
             if(str[i]!=str[j-i-1])
                 return false;
-
         }
-        
-        
         return true;
     }
 };
