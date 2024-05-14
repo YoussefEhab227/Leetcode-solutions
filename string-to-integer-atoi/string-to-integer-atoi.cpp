6147d3d985fa20f10a7a3eea2d54ca6777cb333e
YoussefEhab227 <43238167+YoussefEhab227@users.noreply.github.com>
2024-05-14 07:02:20 +0300

    Time: 0 ms, Memory: 8 MB - LeetHub

--- string-to-integer-atoi/string-to-integer-atoi.cpp
+++ string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -7,37 +7,26 @@ public:
         if(len == 0){
             return 0;
         }
+        int index = 0; char ch;
+        while (s[index] == ' ') ++index;
         
-        //Removing whitespaces
-        int index = 0;
-        while(index < len && s[index] == ' '){
+        if (index == len) return 0;
+        
+        bool isNegative = false;
+        if (s[index] == '-') isNegative =true;
+        if (isNegative || s[index] == '+') ++index;
+        
+        const int maxLimit = INT_MAX/10;
+        int result =0;
+        while (index < len && isDigit(ch=s[index]))
+        {
+            int digit = ch - '0';
+            if(result > maxLimit ||  (result == maxLimit && digit > 7) ) return isNegative? INT_MIN : INT_MAX;
+            
+            result = (10*result) + digit;
             ++index;
         }
-        //if no number after whitespaces return 0
-        if(index == len){
-            return 0;
-        }
-        
-        char ch;
-        bool isNegative = (ch = s[index]) == '-';
-        
-        if(isNegative || ch == '+'){
-            ++index;
-        }
-		
-       const int maxLimit = INT_MAX/10;
-        int result = 0;
-       while (index < len && isDigit ( ch = s[index])) 
-       {
-           int digit = ch - '0';
-           if(result > maxLimit || (result == maxLimit && digit > 7)){
-                return isNegative ? INT_MIN : INT_MAX;
-            }
-           result = digit + (result * 10);
-           ++index;
-       }
-        
-        return isNegative ? -result : result;
+        return isNegative? -result : result;
     }
     
     private:
